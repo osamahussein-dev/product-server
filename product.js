74f8a721d6f8eec3218287bf902f00e4350a8a72
osamahussein-dev <osamahusseindev@gmail.com>
2025-05-17 21:03:04 +0300

    add PUT by id and DELETE by id

--- product.js
+++ product.js
@@ -7,20 +7,37 @@ let products = [
   { id: 2, name: "Phone", price: 800 },
 ];
 
+//GET all
 productsRouter.get("/", (req, res) => {
   res.json(products);
 });
 
+//GET by id
 productsRouter.get("/:id", (req, res) => {
   const product = products.find((p) => p.id == req.params.id);
   res.json(product);
 });
 
+//POST
 productsRouter.post("/", (req, res) => {
   const product = req.body;
   product.id = products.length + 1;
   products.push(product);
   res.json(product);
+});
+
+//PUT by id
+productsRouter.put("/:id", (req, res) => {
+  const product = products.find((p) => p.id == req.params.id);
+  product.name = req.body.name;
+  product.price = req.body.price;
+  res.json(product);
+});
+
+//DELETE by id
+productsRouter.delete("/:id", (req, res) => {
+  products = products.filter((p) => p.id != req.params.id);
+  res.end();
 });
 
 export default productsRouter;
